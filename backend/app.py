4c753a24d52b65231fad4de013ce040888795279
manyabondada <manya.bondada@gmail.com>
2025-02-27 01:37:49 -0600

    raw data added to neon

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,19 @@
-import os
-import psycopg2
-from dotenv import load_dotenv
+import sys
 from flask import Flask
-
-load_dotenv()
+from populate import raw_crisis_nlp_populate
 
 app = Flask(__name__)
-database_url = os.getenv('DATABASE_URL') # Load DB environment variable
-connection = psycopg2.connect(database_url) # Connect to the PostgreSQL database
 
 @app.get("/")
 def home():
     return "Hello, World!"
+
+if __name__ == "__main__":
+    reset_db = "--reset-db" in sys.argv # optional argument to delete and repopulate raw_crisis_nlp_ table
+
+    if reset_db:
+        print("🔄 resetting and repopulating Raw_Crisis_NLP table...")
+    else:
+        print("Checking if Raw_Crisis_NLP table needs to be populated...")
+        
+    raw_crisis_nlp_populate(reset=reset_db)
