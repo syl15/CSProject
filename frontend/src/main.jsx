fa2cd6439c09dd2d9793524d3becfd43c0f63260
Sneha <86683361+snehab2@users.noreply.github.com>
2025-03-12 16:48:14 -0500

    navbar

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './App.css'
 import Navbar from './Navbar.jsx'
 import Dashboard from './Dashboard.jsx'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
 
