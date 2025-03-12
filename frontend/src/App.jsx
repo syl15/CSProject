6f69be3db314e0468f993e23b02e662419450e63
Sneha <86683361+snehab2@users.noreply.github.com>
2025-03-12 17:54:18 -0500

    added navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import { Button } from '@mui/material'
 import Navbar from './Navbar'
-import { StyledEngineProvider } from '@mui/material'
 
-
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <StyledEngineProvider injectFirst>
-      <Navbar />
-    </StyledEngineProvider>
-  )
+      <Navbar/>
+  );
 }
-
-export default App
---
+++ frontend/src/Searchbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { MagnifyingGlassIcon } from '@radix-ui/react-icons'
+
+export default function Searchbar() {
+  return (
+    <div className="relative">
+        <MagnifyingGlassIcon className="absolute left-3 top-[11px]"/>
+        <input
+            className="h-[38px] pl-8 pr-2 border border-[#D4D4D4] rounded-sm text-sm"
+            type="text"
+            id="keyword"
+            placeholder="Search"
+            onfocus="this.placeholder = ''"
+            onblur="this.placeholder = 'enter your text'"
+        />
+    </div>
+
+  )
+}
