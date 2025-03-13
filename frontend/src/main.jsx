4a36c8298f5163ce6e0087cd4904bd34ec6631bb
Sneha <86683361+snehab2@users.noreply.github.com>
2025-03-13 01:20:42 -0500

    created dashboard and added total tweets box

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,13 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './App.css'
 import Navbar from './Navbar.jsx'
-import App from './App.jsx'
-import MobileNavbar from './MobileNavbar.jsx'
+import Dashboard from './Dashboard.jsx'
 
 createRoot(document.getElementById('root')).render(
 
   <StrictMode>
-
     <Navbar/>
+    <Dashboard/>
+
   </StrictMode>
 )
