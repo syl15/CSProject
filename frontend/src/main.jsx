3411b6fa3801350fa2aa6ba38c7500e58703417f
Sneha <86683361+snehab2@users.noreply.github.com>
2025-03-13 00:42:13 -0500

    made navbar responsive -- added mobile navbar

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './App.css'
+import Navbar from './Navbar.jsx'
 import App from './App.jsx'
+import MobileNavbar from './MobileNavbar.jsx'
 
 createRoot(document.getElementById('root')).render(
 
   <StrictMode>
-    <App />
+
+    <Navbar/>
   </StrictMode>
 )
