016ec399cedb5f5b457a0b0be5ac81db98aeb088
Sneha <86683361+snehab2@users.noreply.github.com>
2025-03-13 00:42:13 -0500

    made navbar responsive -- added mobile navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Navbar from './Navbar'
 import MobileNavbar from './MobileNavbar';
 import TestAPI from "./TestAPI.jsx"
+import MobileNavbar from './MobileNavbar';
 
 export default function App() {
   return (
