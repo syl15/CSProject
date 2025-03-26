c2bfea7acd51f96cfe8d77d7b3e69429d5c51515
Sneha Bista <snehabista727@gmail.com>
2025-03-26 14:21:40 -0500

    added top tweets component

---
+++ frontend/src/TopTweets.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import TweetBox from './TweetBox';
+import './App.css'
+
+export default function TopTweets() {
+  return (
+    <div className="flex flex-col flex-start border border-[#D4D4D4] rounded-md mt-5 md:mt-15 text-left p-4 gap-y-2 max-h-80 w-[40rem]">
+        <div className="border-b-1 border-[#D4D4D4]">
+            <h3 className="pb-2">Top Tweets</h3>
+        </div>
+        <div className="flex flex-col pt-4 gap-y-3 overflow-y-scroll scrollbar">
+            <TweetBox/>
+            <TweetBox/>
+            <TweetBox/>
+
+        </div>
+    </div>
+  );
+}
