bad16837bcbfeb41c7fe2fbc84ff3e9f184e63b2
Sneha Bista <snehabista727@gmail.com>
2025-03-26 16:47:24 -0500

    modified width of top tweets component

--- frontend/src/TweetBox.jsx
+++ frontend/src/TweetBox.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default function TweetBox() {
   return (
-    <div className="flex flex-col border border-[#D4D4D4] rounded-xl p-5 mr-5">
+    <div className="flex flex-col border border-[#D4D4D4] rounded-xl p-5 mr-5 w-auto">
         <h4 className="text-md">Jane Doe</h4>
         <p className="text-sm text-[#9D9D9D]">@janedoebluesky</p>
         <div className="flex flex-start flex-wrap pt-4 text-wrap overflow-x-hidden">
