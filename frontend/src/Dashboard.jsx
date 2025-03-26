e348c893120a5598946942ec07ebc5b22085c51f
Sneha Bista <snehabista727@gmail.com>
2025-03-26 14:21:40 -0500

    added top tweets component

--- frontend/src/Dashboard.jsx
+++ frontend/src/Dashboard.jsx
@@ -1,17 +1,23 @@
 import React from 'react'
 import TotalTweets from './TotalTweets'
 import OverallSentiment from './OverallSentiment'
+import Severity from './Severity'
+import TopTweets from './TopTweets'
 
 export default function Dashboard() {
   return (
-    <div className="flex flex-col mt-30">
+    <div className="flex flex-col mt-30 overflow-hidden">
         <div className="flex flex-col gap-y-2">
             <h1 className="text-4xl font-bold text-left">Overview</h1>
             <h3 className="text-md text-left">February 16th, 2025 - February 22nd, 2025</h3>
         </div>
         <div className="row-one flex flex-col md:flex-row md:gap-x-10">
             <TotalTweets/>
-            <OverallSentiment/>
+            {/* <OverallSentiment/> */}
+            <Severity/>
+        </div>
+        <div className="row-three flex flex-col md:flex-row md:gap-x-10">
+            <TopTweets/>
         </div>
         
     </div>
---
+++ frontend/src/Severity.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+export default function Severity() {
+  return (
+    <div className="flex flex-col flex-start border border-[#D4D4D4] rounded-md mt-5 md:mt-15 text-left p-4 gap-y-2">
+        <h3>Severity Level</h3>
+        <h1 className="text-3xl font-bold">Critical</h1>
+    </div>
+  );
+}
