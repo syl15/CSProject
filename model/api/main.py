990f7381917086b42e43d3d3b00fcaef8ef19684
manyabondada <manya.bondada@gmail.com>
2025-03-27 23:28:23 +0530

    model api updates

--- model/api/main.py
+++ model/api/main.py
@@ -6,7 +6,9 @@ import requests
 app = FastAPI() 
 
 # Load dummy model 
-model = joblib.load("optimized_model.sav")
+# TODO: replace line 9 with line 10 when Katrina decodes her ints to strings 
+model = joblib.load("dummy_model.pkl")
+# model = joblib.load("optimized_model.sav")
 
 # Define input schema 
 class TweetInput(BaseModel): 
@@ -27,50 +29,7 @@ def predict_event_type(data: TweetInput):
     text = [data.text]
 
     # Make a prediction 
-    predicted_label = model.predict(text)[0]
+    predicted_label = str(model.predict(text)[0])
 
     # Return event type 
     return {"event_type": predicted_label}
-
-
-# Get posts from bluesky and classify them
-# NOTE: This will be the classification logic 
-FLASK_API_URL = "http://127.0.0.1:5001/unclassified-posts"
-
-@app.get("/bluesky-data") # TODO: change name to /classify 
-def batch_classify(): 
-    """
-    Pulls unclassified posts from Flask, classifies them, and returns predictions
-    """
-    try: 
-        # 1. Get unclassified posts from Flask 
-        response = requests.get(FLASK_API_URL)
-        if response.status_code != 200: 
-            return {"error": "Failed to fetch unclassified posts"}
-
-        posts = response.json() 
-        print(posts)
-        
-        # 2. Run model prediction on each 
-        # NOTE: Run through event classifier AND sentiment classifier before returning
-        '''
-        results = [] 
-        for post in posts: 
-            prediction = model.predict([post["post_original_text"]])[0]
-            results.append({
-                "post_id": post["post_id"], 
-                "post_original_text": post["post_original_text"],
-                "model_disaster_label": prediction
-            })
-        
-        return results
-        '''
-
-        # 3. POST results back to Flask 
-        # TODO: Add a route in Flask to receive classified-posts 
-        # TODO: Add logic here to post final results
-
-        return posts # Return temporarily, return actual results later
-
-    except Exception as e: 
-        return {"error" : str(e)}
