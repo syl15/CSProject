8b65a2a273203bf4dee141fea5fecd38be34ed90
Sneha Bista <snehabista727@gmail.com>
2025-03-27 16:59:48 -0500

    added all disasters page + routing

---
+++ frontend/src/AllDisasters.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+
+export default function AllDisasters() {
+  return (
+    <div className="flex flex-col mt-30 w-screen h-auto absolute left-0 right-0 px-10 md:px-20 pb-10 overflow-x-hidden">
+        <h1 className="text-4xl font-bold text-left">All Disasters</h1>
+    </div>
+  )
+}
