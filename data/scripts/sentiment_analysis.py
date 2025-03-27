a1ec54e0f0211d7a0d59a66b349647dea82d436c
syl15 <listeph15@gmail.com>
2025-03-27 17:56:16 -0500

    update requirements.txt, add run instructions for sentiment_analysis script

--- data/scripts/sentiment_analysis.py
+++ data/scripts/sentiment_analysis.py
@@ -1,14 +1,17 @@
 import pandas as pd
-
-test = pd.read_csv("datasets/test.tsv", sep="\t").sample(n=10)
-
+import nltk
 from sklearn.exceptions import UndefinedMetricWarning
 import warnings
-warnings.filterwarnings("ignore", category=UndefinedMetricWarning)
 
-import nltk
-# nltk.download("vader_lexicon")
+# If you get SSL errors on Mac, run:
+# /Applications/Python\ 3.x/Install\ Certificates.command globally (OUTSIDE your venv)
+# Replace '3.x' with your version (ex. /Applications/Python\ 3.12/Install\ Certificates.command)
+
+nltk.download('vader_lexicon', quiet=True)
 from nltk.sentiment.vader import SentimentIntensityAnalyzer as sia
+
+test = pd.read_csv("../datasets/test.tsv", sep="\t").sample(n=10)
+warnings.filterwarnings("ignore", category=UndefinedMetricWarning)
 
 def analyze_sentiment(text):
     scores = sia().polarity_scores(text)
