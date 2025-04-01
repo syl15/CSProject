73568308725fb6d1483ad826dc262d8de73bcb54
syl15 <listeph15@gmail.com>
2025-04-01 10:36:38 -0500

    refactor model_helper.py, update fastapi with new sentiment score format

--- model/api/model_helpers.py
+++ model/api/model_helpers.py
@@ -1,6 +1,40 @@
 import re
+import spacy
+import spacy.util 
+import subprocess 
+import sys
+import pandas as pd
+import nltk
+from sklearn.exceptions import UndefinedMetricWarning
+import warnings
 
+# ------------------ Setup ------------------
+
+# Installs 'en_core_web_sm' if it is already not
+def install_spacy_model(model='en_core_web_sm'): 
+    try: 
+        spacy.load(model) 
+    except OSError: 
+        subprocess.run([sys.executable, "-m", "spacy", "download", model], check=True)
+
+# Install and load the model 
+install_spacy_model() 
 nlp = spacy.load('en_core_web_sm')
+
+# If you get SSL errors on Mac, run:
+# /Applications/Python\ 3.x/Install\ Certificates.command globally (OUTSIDE your venv)
+# Replace '3.x' with your version (ex. /Applications/Python\ 3.12/Install\ Certificates.command)
+
+# Download VADER lexicon 
+nltk.download('vader_lexicon', quiet=True)
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+warnings.filterwarnings("ignore", category=UndefinedMetricWarning)
+
+# Initialize the sentiment analyzer
+sia = SentimentIntensityAnalyzer()
+
+# ------------------ Preprocessing ------------------
+
 def preprocess(text):
     text = re.sub(r'https?:\/\/\S*', '', text, flags=re.MULTILINE)
     text = re.sub(r'[^\w\s]', ' ', text)
@@ -9,28 +43,15 @@ def preprocess(text):
     text = " ".join([token.lemma_ for token in nlp(text)])
     return text
 
-import pandas as pd
-import nltk
-from sklearn.exceptions import UndefinedMetricWarning
-import warnings
-
-# If you get SSL errors on Mac, run:
-# /Applications/Python\ 3.x/Install\ Certificates.command globally (OUTSIDE your venv)
-# Replace '3.x' with your version (ex. /Applications/Python\ 3.12/Install\ Certificates.command)
-
-nltk.download('vader_lexicon', quiet=True)
-from nltk.sentiment.vader import SentimentIntensityAnalyzer
-warnings.filterwarnings("ignore", category=UndefinedMetricWarning)
-
-# Initialize the analyzer once
-sia = SentimentIntensityAnalyzer()
+# ------------------ Sentiment Analysis ------------------
 
 def analyze_sentiment(text):
     scores = sia.polarity_scores(text)
     return scores["compound"]
 
-###### Test the sentiment analysis model ######
-# test = pd.read_csv("../datasets/test.tsv", sep="\t").sample(n=10)
-# for i in test["tweet_text"]:
-#     score = analyze_sentiment(i)
-#     print(score, i)
+# ------------------ Testing Locally ------------------
+if __name__ == "__main__":
+    posts = pd.read_csv("../../data/datasets/test.tsv", sep="\t").sample(n=10)
+    for post_text in posts["tweet_text"]:
+        score = analyze_sentiment(preprocess(post_text))
+        print(score, post_text)
