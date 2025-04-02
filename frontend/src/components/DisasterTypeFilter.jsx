2c537e6702084394bd7d7c10e9da5ccfd0973625
Sneha <86683361+snehab2@users.noreply.github.com>
2025-04-02 13:11:20 -0500

    updated filters to collapsible

--- frontend/src/components/DisasterTypeFilter.jsx
+++ frontend/src/components/DisasterTypeFilter.jsx
@@ -5,33 +5,35 @@ import {
 } from "@radix-ui/react-icons";
 import { useState } from "react";
 
-export default function TimeFilter() {
+export default function DisasterTypeFilter() {
     const [open, setOpen] = useState(false);
 	return (
-        
 		<Collapsible.Root
-			className="relative"
+			className="CollapsibleRoot"
 			open={open}
 			onOpenChange={setOpen}
 		>
+	
 		<Collapsible.Trigger className="flex flex-row justify-center items-center gap-x-2 border border-[#D4D4D4] rounded-md bg-white">
-			Time
+			Disaster Type
 			{open ? <Cross2Icon /> : <CaretDownIcon />}
 		</Collapsible.Trigger>
-       
-        
-		<Collapsible.Content className="flex flex-col flex-start border border-[#D4D4D4] rounded-md gap-y-2 text-left p-4 mt-3 ">
+
+		<Collapsible.Content className="flex flex-col flex-start border border-[#D4D4D4] rounded-md gap-y-2 text-left p-4 mt-3">
 			<div className="Repository">
-				<span className="Text">All time</span>
+				<span className="Text">All types</span>
 			</div>
 			<div className="Repository">
-				<span className="Text">Past 1 year</span>
+				<span className="Text">Hurricane</span>
 			</div>
             <div className="Repository">
-				<span className="Text">Past 5 years</span>
+				<span className="Text">Flood</span>
 			</div>
             <div className="Repository">
-				<span className="Text">Past 10 years</span>
+				<span className="Text">Earthquakes</span>
+			</div>
+            <div className="Repository">
+				<span className="Text">Fire</span>
 			</div>
 			</Collapsible.Content>
 		</Collapsible.Root>
