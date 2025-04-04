6bc0c7f7f4a457faed16fab9599979ea1a2eb3de
Sneha Bista <snehabista727@gmail.com>
2025-04-03 20:31:00 -0500

    updated main.py

--- model/api/main.py
+++ model/api/main.py
@@ -13,8 +13,10 @@ LABEL_MAP = {
 
 app = FastAPI() 
 
-# Load disaster-type classifier
-model = joblib.load("optimized_model.sav")
+# Load dummy model 
+# TODO: replace line 9 with line 10 when Katrina decodes her ints to strings 
+model = joblib.load("dummy_model.pkl")
+# model = joblib.load("optimized_model.sav")
 
 # Define input schema 
 class PostInput(BaseModel): 
@@ -37,15 +39,50 @@ def predict_event_type(data: PostInput):
     text = [preprocess(data.text)]
 
     # Make a prediction 
-    predicted_class = model.predict(text)[0] # Returns value from [0...4]
-    predicted_label = LABEL_MAP.get(int(predicted_class), "unknown") # Convert class to string value
+    predicted_label = str(model.predict(text)[0])
 
     # Return event type 
     return {"event_type": predicted_label}
 
-@app.post("/predict-sentiment", response_model=SentimentOutput)
-def predict_sentiment(data: PostInput): 
-    score = analyze_sentiment(preprocess(data.text))
 
-    # Return sentiment_score 
-    return {"sentiment_score": score}
+# Get posts from bluesky and classify them
+# NOTE: This will be the classification logic 
+FLASK_API_URL = "http://127.0.0.1:5001/unclassified-posts"
+
+@app.get("/bluesky-data") # TODO: change name to /classify 
+def batch_classify(): 
+    """
+    Pulls unclassified posts from Flask, classifies them, and returns predictions
+    """
+    try: 
+        # 1. Get unclassified posts from Flask 
+        response = requests.get(FLASK_API_URL)
+        if response.status_code != 200: 
+            return {"error": "Failed to fetch unclassified posts"}
+
+        posts = response.json() 
+        print(posts)
+        
+        # 2. Run model prediction on each 
+        # NOTE: Run through event classifier AND sentiment classifier before returning
+        '''
+        results = [] 
+        for post in posts: 
+            prediction = model.predict([post["post_original_text"]])[0]
+            results.append({
+                "post_id": post["post_id"], 
+                "post_original_text": post["post_original_text"],
+                "model_disaster_label": prediction
+            })
+        
+        return results
+        '''
+
+        # 3. POST results back to Flask 
+        # TODO: Add a route in Flask to receive classified-posts 
+        # TODO: Add logic here to post final results
+
+        return posts # Return temporarily, return actual results later
+
+    except Exception as e: 
+        return {"error" : str(e)}
