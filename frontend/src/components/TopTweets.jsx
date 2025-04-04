8879c68c93df292d53789e22dcfc332f0b1f3b28
Sneha Bista <snehabista727@gmail.com>
2025-04-03 19:18:54 -0500

    parsed mock data api

--- frontend/src/components/TopTweets.jsx
+++ frontend/src/components/TopTweets.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 import TweetBox from './TweetBox';
 
 
-export default function TopTweets() {
+export default function TopTweets({tweetsList}) {
   return (
     <div className="flex flex-col border border-[#D4D4D4] rounded-md mt-5 md:mt-15 text-left p-4 gap-y-2 h-[25rem] w-full">
         <div className="border-b-1 border-[#D4D4D4]">
             <h3 className="pb-2">Top Tweets</h3>
         </div>
         <div className="flex flex-col pt-4 gap-y-3 overflow-y-scroll scrollbar">
-            <TweetBox/>
-            <TweetBox/>
-            <TweetBox/>
+            {tweetsList.map((tweet) => (
+              <TweetBox tweetObj={tweet}/>
+            ))}
         </div>
     </div>
   );
