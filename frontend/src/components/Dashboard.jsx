082e90738525d0367afe5902e35a2dbdc0bfbf08
Sneha Bista <snehabista727@gmail.com>
2025-04-03 22:47:24 -0500

    increased bottom padding for generic dashboard

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -10,7 +10,7 @@ export default function Dashboard({disaster}) {
     const start = new Date(`${disaster.startDate}T00:00:00`).toLocaleDateString("en-US", { month: "short", day: "numeric", year: "numeric"});
     const end = new Date(`${disaster.endDate}T00:00:00`).toLocaleDateString("en-US", { month: "short", day: "numeric", year: "numeric"});
   return (
-    <div className="flex flex-col mt-30 w-screen h-auto absolute left-0 right-0 px-10 md:px-20 pb-10 overflow-x-hidden">
+    <div className="flex flex-col mt-30 w-screen h-auto absolute left-0 right-0 px-10 md:px-20 pb-10 md:pb-40 overflow-x-hidden">
         <div className="flex flex-col gap-y-2">
             <h1 className="text-4xl font-bold text-left">{disaster.name}</h1>
             <h3 className="text-lg text-left">{start} - {end}</h3>
