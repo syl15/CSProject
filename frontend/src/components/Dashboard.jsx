eebcc4097ddd67f7fe940fe5f256aa305a2ff0eb
Sneha Bista <snehabista727@gmail.com>
2025-04-07 17:27:39 -0500

    updated routing for all disasters page

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -5,27 +5,30 @@ import TopTweets from './TopTweets'
 import Summary from './Summary'
 import Map from './Map'
 import PieChart from './PieChart'
+import { useLocation } from 'react-router-dom'
 
 export default function Dashboard({disaster}) {
-    const start = new Date(`${disaster.startDate}T00:00:00`).toLocaleDateString("en-US", { month: "short", day: "numeric", year: "numeric"});
-    const end = new Date(`${disaster.endDate}T00:00:00`).toLocaleDateString("en-US", { month: "short", day: "numeric", year: "numeric"});
+    const location = useLocation();
+    const currDisaster = location.state || disaster; 
+    const start = new Date(`${currDisaster.startDate}T00:00:00`).toLocaleDateString("en-US", { month: "short", day: "numeric", year: "numeric"});
+    const end = new Date(`${currDisaster.endDate}T00:00:00`).toLocaleDateString("en-US", { month: "short", day: "numeric", year: "numeric"});
   return (
     <div className="flex flex-col mt-30 w-screen h-auto absolute left-0 right-0 px-10 md:px-20 pb-10 md:pb-40 overflow-x-hidden">
         <div className="flex flex-col gap-y-2">
-            <h1 className="text-4xl font-bold text-left">{disaster.name}</h1>
+            <h1 className="text-4xl font-bold text-left">{currDisaster.name}</h1>
             <h3 className="text-lg text-left">{start} - {end}</h3>
         </div>
         <div className="row-one flex flex-col lg:flex-row lg:gap-x-10">
-            <TotalTweets total={disaster.totalTweets}/>
-            <Severity severity={disaster.severity}/>
+            <TotalTweets total={currDisaster.totalTweets}/>
+            <Severity severity={currDisaster.severity}/>
         </div>
         <div className="row-two flex flex-col lg:flex-row lg:gap-x-10">
-            <Summary summary={disaster.summary}/>
-            <PieChart sentiment={disaster.sentiment}/>
+            <Summary summary={currDisaster.summary}/>
+            <PieChart sentiment={currDisaster.sentiment}/>
         </div>
         <div className="row-three flex flex-col overflow-hidden w-full lg:flex-row md:gap-x-10">
-            <Map location={disaster.location}/>
-            <TopTweets tweetsList={disaster.topTweets}/>
+            <Map location={currDisaster.location}/>
+            <TopTweets tweetsList={currDisaster.topTweets}/>
         </div>
     </div>
   )
