e79693ae59731b6bca815fe678381854e736317a
Sneha Bista <snehabista727@gmail.com>
2025-04-07 17:27:39 -0500

    updated routing for all disasters page

--- frontend/src/components/Summary.jsx
+++ frontend/src/components/Summary.jsx
@@ -1,15 +1,13 @@
 import React from 'react'
 
-export default function Summary() {
+export default function Summary({summary}) {
   return (
     <div className="flex flex-col flex-start border border-[#D4D4D4] rounded-md mt-5 md:mt-15 text-left p-4 gap-y-2 max-w-xl">
         <div className="border-b-1 border-[#D4D4D4]">
             <h3 className="pb-2">Summary</h3>
         </div>
         <div className="flex flex-wrap p-2">
-        The California Wildfires happened from January 7th, 2025 to January 31st 2025. The most affected regions are: Pacific Palisades, Malibu, Topanga, Pasadena, Altadena, Sierra Madre, and La Canada Flintridge.
-
-        Many tweets express sadness, frustration, and grief. There’s strong support for firefighters and calls for donations to help victims.
+          {summary}
         </div>
     </div>
   );
--- frontend/src/components/TopTweets.jsx
+++ frontend/src/components/TopTweets.jsx
@@ -6,7 +6,7 @@ export default function TopTweets({tweetsList}) {
   return (
     <div className="flex flex-col border border-[#D4D4D4] rounded-md mt-5 md:mt-15 text-left p-4 gap-y-2 h-[25rem] w-full">
         <div className="border-b-1 border-[#D4D4D4]">
-            <h3 className="pb-2">Top Tweets</h3>
+            <h3 className="pb-2">Tweets</h3>
         </div>
         <div className="flex flex-col pt-4 gap-y-3 overflow-y-scroll scrollbar">
             {tweetsList.map((tweet) => (
