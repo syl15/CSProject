2c95b4ed29fc359605997cc1bed4a3fa3200f237
Sneha Bista <snehabista727@gmail.com>
2025-04-07 17:27:39 -0500

    updated routing for all disasters page

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -5,31 +5,32 @@ import TopTweets from './TopTweets'
 import Summary from './Summary'
 import Map from './Map'
 import PieChart from './PieChart'
-import AddedDate from './AddedDate'
 import { useLocation } from 'react-router-dom'
 
 export default function Dashboard({disaster}) {
     const location = useLocation();
     const currDisaster = location.state || disaster; 
     const start = new Date(`${currDisaster.startDate}T00:00:00`).toLocaleDateString("en-US", { month: "short", day: "numeric", year: "numeric"});
-    return (
+    const end = new Date(`${currDisaster.endDate}T00:00:00`).toLocaleDateString("en-US", { month: "short", day: "numeric", year: "numeric"});
+  return (
     <div className="flex flex-col mt-30 w-screen h-auto absolute left-0 right-0 px-10 md:px-20 pb-10 md:pb-40 overflow-x-hidden">
         <div className="flex flex-col gap-y-2">
             <h1 className="text-4xl font-bold text-left">{currDisaster.name}</h1>
-            <h3 className="text-lg text-left">{start}</h3>
+            <h3 className="text-lg text-left">{start} - {end}</h3>
         </div>
         <div className="row-one flex flex-col lg:flex-row lg:gap-x-10">
-            <TotalTweets total={currDisaster.totalPosts}/>
+            <TotalTweets total={currDisaster.totalTweets}/>
             <Severity severity={currDisaster.severity}/>
-            <AddedDate dateAdded={start}/>
         </div>
         <div className="row-two flex flex-col lg:flex-row lg:gap-x-10">
+            <Summary summary={currDisaster.summary}/>
+            <PieChart sentiment={currDisaster.sentiment}/>
             <Summary summary={currDisaster.summary}/>
             <PieChart sentiment={currDisaster.sentiment}/>
         </div>
         <div className="row-three flex flex-col overflow-hidden w-full lg:flex-row md:gap-x-10">
             <Map location={currDisaster.location}/>
-            <TopTweets tweetsList={currDisaster.posts}/>
+            <TopTweets tweetsList={currDisaster.topTweets}/>
         </div>
     </div>
   )
