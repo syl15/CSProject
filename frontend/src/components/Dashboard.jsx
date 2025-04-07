74b4a7c85514a9c307714f4db99a8f63228a4415
Sneha Bista <snehabista727@gmail.com>
2025-04-07 17:27:39 -0500

    updated routing for all disasters page

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -15,11 +15,11 @@ export default function Dashboard({disaster}) {
     <div className="flex flex-col mt-30 w-screen h-auto absolute left-0 right-0 px-10 md:px-20 pb-10 md:pb-40 overflow-x-hidden">
         <div className="flex flex-col gap-y-2">
             <h1 className="text-4xl font-bold text-left">{currDisaster.name}</h1>
-            <h3 className="text-lg text-left">{start}</h3>
+            <h3 className="text-lg text-left">{start} - {end}</h3>
         </div>
         <div className="row-one flex flex-col lg:flex-row lg:gap-x-10">
-            <TotalTweets total={currDisaster.totalPosts}/>
-            {/* <Severity severity={currDisaster.severity}/> */}
+            <TotalTweets total={currDisaster.totalTweets}/>
+            <Severity severity={currDisaster.severity}/>
         </div>
         <div className="row-two flex flex-col lg:flex-row lg:gap-x-10">
             <Summary summary={currDisaster.summary}/>
@@ -27,12 +27,7 @@ export default function Dashboard({disaster}) {
         </div>
         <div className="row-three flex flex-col overflow-hidden w-full lg:flex-row md:gap-x-10">
             <Map location={currDisaster.location}/>
-            {currDisaster.posts ? (
-                <TopTweets tweetsList={currDisaster.posts}/>
-            ) : (
-                <p>No data</p>
-            )}
-            {/* <TopTweets tweetsList={currDisaster.topTweets}/> */}
+            <TopTweets tweetsList={currDisaster.topTweets}/>
         </div>
     </div>
   )
