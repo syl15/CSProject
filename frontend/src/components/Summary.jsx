8db3bb95f9ebc63e6b43da775943254134870897
Sneha Bista <snehabista727@gmail.com>
2025-04-07 17:27:39 -0500

    updated routing for all disasters page

--- frontend/src/components/Summary.jsx
+++ frontend/src/components/Summary.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function Summary() {
+export default function Summary({summary}) {
   return (
     <div className="flex flex-col flex-start border border-[#D4D4D4] rounded-md mt-5 md:mt-15 text-left p-4 gap-y-2 max-w-xl lg:w-xl">
         <div className="border-b-1 border-[#D4D4D4]">
