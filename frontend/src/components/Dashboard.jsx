96e9a5b1d50b2735192adac94d663de32a398268
Sneha Bista <snehabista727@gmail.com>
2025-04-07 17:27:39 -0500

    updated routing for all disasters page

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -5,48 +5,31 @@ import TopTweets from './TopTweets'
 import Summary from './Summary'
 import Map from './Map'
 import PieChart from './PieChart'
-import AddedDate from './AddedDate'
 import { useLocation } from 'react-router-dom'
-import { useState, useEffect } from 'react'
-const BASE_URL = "http://127.0.0.1:5001"; // Flask API URL
 
 export default function Dashboard({disaster}) {
     const location = useLocation();
     const currDisaster = location.state || disaster; 
-   
-   const [disasterInfo, setDisasterInfo] = useState();
-   // Fetch disasters on mount 
-    useEffect(() => {
-        // Add parameters if necessary
-        fetch(`${BASE_URL}/disasters/${currDisaster.id}`) // Ex. /disasters?limit=1 
-            .then((result) => result.json())
-            .then(setDisasterInfo)
-            .catch(console.error);
-    }, [])
-   
-    if(!disasterInfo) {
-        return <p>Loading...</p>
-    }
-    const start = new Date(`${disasterInfo.startDate}T00:00:00`).toLocaleDateString("en-US", { month: "short", day: "numeric", year: "numeric"});
-
-    return (
-        <div className="flex flex-col mt-30 w-screen h-auto absolute left-0 right-0 px-10 md:px-20 pb-10 md:pb-40 overflow-x-hidden">
-            <div className="flex flex-col gap-y-2">
-                <h1 className="text-4xl font-bold text-left">{disasterInfo.name}</h1>
-            </div>
-            <div className="row-one flex flex-col lg:flex-row lg:gap-x-10">
-                <AddedDate dateAdded={start}/>
-                <TotalTweets total={disasterInfo.totalPosts}/>
-                <Severity severity={disasterInfo.severity}/>
-            </div>
-            <div className="row-two flex flex-col lg:flex-row lg:gap-x-10">
-                <Summary summary={disasterInfo.summary}/>
-                <PieChart sentiment={disasterInfo.sentiment}/>
-            </div>
-            <div className="row-three flex flex-col overflow-hidden w-full lg:flex-row md:gap-x-10">
-                <Map location={disasterInfo.location}/>
-                <TopTweets tweetsList={disasterInfo.posts}/>
-            </div>
+    const start = new Date(`${currDisaster.startDate}T00:00:00`).toLocaleDateString("en-US", { month: "short", day: "numeric", year: "numeric"});
+    const end = new Date(`${currDisaster.endDate}T00:00:00`).toLocaleDateString("en-US", { month: "short", day: "numeric", year: "numeric"});
+  return (
+    <div className="flex flex-col mt-30 w-screen h-auto absolute left-0 right-0 px-10 md:px-20 pb-10 md:pb-40 overflow-x-hidden">
+        <div className="flex flex-col gap-y-2">
+            <h1 className="text-4xl font-bold text-left">{currDisaster.name}</h1>
+            <h3 className="text-lg text-left">{start} - {end}</h3>
         </div>
-    )
+        <div className="row-one flex flex-col lg:flex-row lg:gap-x-10">
+            <TotalTweets total={currDisaster.totalTweets}/>
+            <Severity severity={currDisaster.severity}/>
+        </div>
+        <div className="row-two flex flex-col lg:flex-row lg:gap-x-10">
+            <Summary summary={currDisaster.summary}/>
+            <PieChart sentiment={currDisaster.sentiment}/>
+        </div>
+        <div className="row-three flex flex-col overflow-hidden w-full lg:flex-row md:gap-x-10">
+            <Map location={currDisaster.location}/>
+            <TopTweets tweetsList={currDisaster.topTweets}/>
+        </div>
+    </div>
+  )
 }
