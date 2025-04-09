e49efaed242c31cbd10db7850978453178e2cab6
Sneha Bista <snehabista727@gmail.com>
2025-04-09 13:53:36 -0500

    added functionality to filters and redid filter ui

--- frontend/src/AllDisasters.jsx
+++ frontend/src/AllDisasters.jsx
@@ -16,6 +16,7 @@ const BASE_URL = "http://127.0.0.1:5001"; // Flask API URL
 export default function AllDisasters() {
     const [disasters, setDisasters] = useState([]);
     const [currIndex, setCurrIndex] = useState(0);
+    const [filteredDisasters, setFilteredDisasters] = useState([]);
     const navigate = useNavigate();
 
     // Fetch disasters on mount 
@@ -23,11 +24,15 @@ export default function AllDisasters() {
         // Add parameters if necessary
         fetch(`${BASE_URL}/disasters`) // Ex. /disasters?limit=1 
             .then((result) => result.json())
-            .then(setDisasters)
+            .then((result) => {
+              setDisasters(result);
+              setFilteredDisasters(result);
+            })
             .catch(console.error);
     }, [])
 
-    if(!disasters) {
+
+    if(!disasters && !filteredDisasters) {
       return <div>Loading...</div>
     }
 
@@ -46,17 +51,42 @@ export default function AllDisasters() {
         setCurrIndex(currIndex + 3);
       }
     };
-    
-    
+
+
+    const positiveSelected = (e) => {
+      if(e.target.checked){
+        const newDisasters = disasters.filter(disaster => 
+          disaster.name === e.target.value
+        );
+        setFilteredDisasters(newDisasters)
+      }
+      else {
+        setFilteredDisasters(disasters);
+      }
+    }
 
     return (
       <div className="flex flex-col mt-30 min-w-screen min-h-screen h-auto absolute left-0 right-0 px-10 md:px-20 pb-10 overflow-x-hidden">
           <h1 className="text-4xl font-bold text-left">All Disasters</h1>
-          <div className="flex flex-col gap-y-10 md:flex-row w-full">
-              <FilterColumn/>
+          <div className="flex flex-col gap-y-10 md:flex-row w-full gap-x-10">
+          <div className="flex flex-col items-start gap-x-2 w-1/2 gap-y-3 mt-20">
+
+            <h3 className="border border-[#D4D4D4] rounded-md text-md py-2 px-4">Overall Sentiment</h3>
+            <div className="flex flex-row items-center gap-x-2">
+              <input
+                type="checkbox"
+                value="Florida Hurricane"
+                onChange={positiveSelected}
+                id="positive"
+              />
+              <p>Positive</p>
+            </div>
+          
+          </div>
+          
               
               <div className="flex flex-col w-full md:mt-15">
-                {disasters.slice(currIndex,currIndex+3).map((disaster) => (
+                {filteredDisasters.slice(currIndex,currIndex+3).map((disaster) => (
                   <div key={disaster.id} className="mb-5 rounded-md cursor-pointer max-w-xl hover:bg-gray-100" onClick={() => navigateToDisaster(disaster)}>
                       <DisasterCard disaster={disaster}/>
                   </div>
