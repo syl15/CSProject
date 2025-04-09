ebd60a7d401960a4f4c6164ebae08aea6b78451f
Sneha Bista <snehabista727@gmail.com>
2025-04-09 18:05:07 -0500

    update api stuff and checkboxes

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -11,16 +11,14 @@ export default function Dashboard({disaster}) {
     const location = useLocation();
     const currDisaster = location.state || disaster; 
     const start = new Date(`${currDisaster.startDate}T00:00:00`).toLocaleDateString("en-US", { month: "short", day: "numeric", year: "numeric"});
-    const end = new Date(`${currDisaster.endDate}T00:00:00`).toLocaleDateString("en-US", { month: "short", day: "numeric", year: "numeric"});
-
   return (
     <div className="flex flex-col mt-30 w-screen h-auto absolute left-0 right-0 px-10 md:px-20 pb-10 md:pb-40 overflow-x-hidden">
         <div className="flex flex-col gap-y-2">
             <h1 className="text-4xl font-bold text-left">{currDisaster.name}</h1>
-            <h3 className="text-lg text-left">{start} - {end}</h3>
+            <h3 className="text-lg text-left">{start}</h3>
         </div>
         <div className="row-one flex flex-col lg:flex-row lg:gap-x-10">
-            <TotalTweets total={currDisaster.totalTweets}/>
+            <TotalTweets total={currDisaster.totalPosts}/>
             {/* <Severity severity={currDisaster.severity}/> */}
         </div>
         <div className="row-two flex flex-col lg:flex-row lg:gap-x-10">
@@ -31,9 +29,12 @@ export default function Dashboard({disaster}) {
         </div>
         <div className="row-three flex flex-col overflow-hidden w-full lg:flex-row md:gap-x-10">
             <Map location={currDisaster.location}/>
-            <TopTweets tweetsList={currDisaster.topTweets}/>
-            <Map location={currDisaster.location}/>
-            <TopTweets tweetsList={currDisaster.topTweets}/>
+            {currDisaster.posts ? (
+                <TopTweets tweetsList={currDisaster.posts}/>
+            ) : (
+                <p>No data</p>
+            )}
+            {/* <TopTweets tweetsList={currDisaster.topTweets}/> */}
         </div>
     </div>
   )
