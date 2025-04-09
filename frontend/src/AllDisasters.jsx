35364914309780891a3a6e38d98a31e24a89b303
Sneha Bista <snehabista727@gmail.com>
2025-04-08 20:23:45 -0500

    added pagnination

--- frontend/src/AllDisasters.jsx
+++ frontend/src/AllDisasters.jsx
@@ -10,7 +10,9 @@ const BASE_URL = "http://127.0.0.1:5001"; // Flask API URL
 
 export default function AllDisasters() {
     const [disasters, setDisasters] = useState([]);
+    const [currIndex, setCurrIndex] = useState(0);
     const navigate = useNavigate();
+
     // Fetch disasters on mount 
     useEffect(() => {
         // Add parameters if necessary
@@ -26,22 +28,42 @@ export default function AllDisasters() {
 
     const navigateToDisaster = (disaster) => {
       navigate(`/AllDisasters/${disaster.name}`, { state: disaster });
-    };
+    }
 
+    const back = () => {
+      if(currIndex > 0) {
+        setCurrIndex(currIndex-3);
+      }
+    }
+
+    const next = () => {
+      if (currIndex < disasters.length - 1) {
+        setCurrIndex(currIndex + 3);
+      }
+    };
+    
+    
 
     return (
       <div className="flex flex-col mt-30 min-w-screen min-h-screen h-auto absolute left-0 right-0 px-10 md:px-20 pb-10 overflow-x-hidden">
           <h1 className="text-4xl font-bold text-left">All Disasters</h1>
           <div className="flex flex-col gap-y-10 md:flex-row w-full">
               <FilterColumn/>
+              
               <div className="flex flex-col w-full md:mt-15">
-                {disasters.map((disaster) => (
+                {disasters.slice(currIndex,currIndex+3).map((disaster) => (
                   <div key={disaster.id} className="mb-5 rounded-md cursor-pointer max-w-xl hover:bg-gray-100" onClick={() => navigateToDisaster(disaster)}>
                       <DisasterCard disaster={disaster}/>
                   </div>
                   
                 ))}
+                <div className="flex flex-row gap-x-3 mt-3 relative">
+                  <button onClick={back} className="border border-[#D4D4D4] rounded-md">Previous</button>
+                  <button onClick={next} className="border border-[#D4D4D4] rounded-md">Next</button>
+                </div>
               </div>
+            
+              
           </div>
 
           
