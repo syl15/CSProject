dc689b3b6475b4ad46987da4e41fde46a2aaf2f2
Sneha Bista <snehabista727@gmail.com>
2025-04-10 12:22:27 -0500

    updated total posts title

--- frontend/src/components/TotalTweets.jsx
+++ frontend/src/components/TotalTweets.jsx
@@ -4,7 +4,7 @@ export default function TotalTweets({total}) {
   console.log
   return (
     <div className="flex flex-col flex-start border border-[#D4D4D4] rounded-md mt-15 text-left p-4">
-        <h3>Total Tweets</h3>
+        <h3>Total Posts</h3>
         <h1 className="text-3xl font-bold mt-2">{total}</h1>
     </div>
   )
