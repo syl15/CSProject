dfdf4a858b1913c9936916e9a0b7f61268f09571
Sneha Bista <snehabista727@gmail.com>
2025-04-11 12:31:04 -0500

    fixed formatting and added comments

--- frontend/src/AllDisasters.jsx
+++ frontend/src/AllDisasters.jsx
@@ -132,6 +132,8 @@ export default function AllDisasters() {
       
     };
 
+
+
     return (
       <div className="flex flex-col mt-30 min-w-screen min-h-screen h-auto absolute left-0 right-0 px-10 md:px-20 pb-10 overflow-x-hidden">
           <h1 className="text-4xl font-bold text-left">All Disasters</h1>
