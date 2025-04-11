548c334057b5e1651b747bf67f0307cf7a1b4eb0
Sneha Bista <snehabista727@gmail.com>
2025-04-11 11:24:58 -0500

    updated disaster event and sentiment filter

--- frontend/src/AllDisasters.jsx
+++ frontend/src/AllDisasters.jsx
@@ -5,6 +5,10 @@ import FilterColumn from './components/FilterColumn'
 import { useState, useEffect } from 'react'
 import { useNavigate, Link } from 'react-router-dom';
 import Dashboard from './components/Dashboard'
+import { Collapsible } from "radix-ui";
+import {
+	CaretDownIcon, CheckIcon, Cross2Icon, RowSpacingIcon
+} from "@radix-ui/react-icons";
 
 const BASE_URL = "http://127.0.0.1:5001"; // Flask API URL
 
@@ -12,7 +16,13 @@ export default function AllDisasters() {
     const [disasters, setDisasters] = useState([]);
     const [currIndex, setCurrIndex] = useState(0);
     const [filteredDisasters, setFilteredDisasters] = useState([]);
-    const [filterSelected, setFilterSelected] = useState([]);
+    const [sentimentFilter, setSentimentFilter] = useState([]);
+    const [eventFilter, setEventFilter] = useState([]); 
+    const [openSentiment, setOpenSentiment] = useState(false);
+    const [openEvent, setOpenEvent] = useState(false);
+    const sentimentTypes = ["positive", "neutral", "negative"];
+    const eventTypes = ["earthquake", "hurricane", "wildfire"]
+    
     const navigate = useNavigate();
 
     // Fetch disasters on mount 
@@ -49,66 +59,117 @@ export default function AllDisasters() {
       }
     };
 
-    const filterChange = (e) => {
+    const filterUpdate = (e, filters, setFilters) => {
       if(e.target.checked) {
-        setFilterSelected([...filterSelected, e.target.value])
+        setFilters([...filters, e.target.value])
       } else {
-        setFilterSelected(filterSelected.filter((filterID) => filterID !== e.target.value))
+        setFilters(filters.filter((filterID) => filterID !== e.target.value))
       }
     }
 
     useEffect(() => {
-      if(filterSelected.length === 0) {
+      if(sentimentFilter.length === 0 && eventFilter.length === 0) {
         setFilteredDisasters(disasters);
       } 
       else {
-        const newDisasters = filterSelected.map((filterID) =>
-          disasters.filter(disaster => disaster.overallSentiment === filterID)
-        );
-        setFilteredDisasters(
-          newDisasters.flat().sort(function(a, b) {
-            return a.id - b.id;
-          })
-        );
+        let resultArr = disasters;
+        resultArr = filterBySentiment(resultArr);
+        console.log(resultArr);
+        resultArr = filterByEvent(resultArr.flat());
+        console.log(resultArr);
+        setFilteredDisasters(resultArr.flat().sort(function(a, b) {
+          return a.id - b.id;
+        }));
+
       }
-    }, [filterSelected])
+    }, [sentimentFilter, eventFilter])
+
+    const filterBySentiment = (filteredArray) => {
+      if(sentimentFilter.length === 0) {
+        return filteredArray;
+      }
+      else {
+        filteredArray = sentimentFilter.map((filterID) => (
+          filteredArray.filter(disaster => disaster.overallSentiment === filterID)
+        ))        
+        return filteredArray;
+      }
+      
+    };
+
+    const filterByEvent = (filteredArray) => {
+      if(eventFilter.length === 0) {
+        return filteredArray;
+      }
+      else {
+        
+        filteredArray = eventFilter.map((filterID) => (
+          filteredArray.filter(disaster => disaster.eventType === filterID)
+        ))
+        return filteredArray;
+      }
+      
+    };
 
     return (
       <div className="flex flex-col mt-30 min-w-screen min-h-screen h-auto absolute left-0 right-0 px-10 md:px-20 pb-10 overflow-x-hidden">
           <h1 className="text-4xl font-bold text-left">All Disasters</h1>
           <div className="flex flex-col gap-y-10 md:flex-row w-full gap-x-10">
           <div className="flex flex-col items-start gap-x-2 w-1/2 gap-y-3 mt-20">
+            <Collapsible.Root
+              className="relative"
+              open={openSentiment}
+              onOpenChange={setOpenSentiment}
+            >
+              <Collapsible.Trigger className="flex flex-row justify-center items-center gap-x-2 border border-[#D4D4D4] rounded-md bg-white">
+              Overall Sentiment
+                {openSentiment ? <Cross2Icon /> : <CaretDownIcon />}
+              </Collapsible.Trigger>
+                
+                  
+              <Collapsible.Content className="flex flex-col flex-start border border-[#D4D4D4] rounded-md gap-y-1 text-left p-4 mt-3 ">
+                {sentimentTypes.map((sentimentID) => (
+                  <div className="flex flex-row items-center gap-x-2">
+                    <input
+                      type="checkbox"
+                      value={sentimentID}
+                      onChange={(e) => filterUpdate(e, sentimentFilter, setSentimentFilter)}
+                      id={sentimentID}
+                    />
+                    <p>{sentimentID}</p>
+                  </div>
+                ))}
+              </Collapsible.Content>
+            </Collapsible.Root>    
 
-            <h3 className="border border-[#D4D4D4] rounded-md text-md py-2 px-4">Overall Sentiment</h3>
-            <div className="flex flex-row items-center gap-x-2">
-              <input
-                type="checkbox"
-                value="positive"
-                onChange={filterChange}
-                id="positive"
-              />
-              <p>Positive</p>
+            
+            <Collapsible.Root
+              className="relative"
+              open={openEvent}
+              onOpenChange={setOpenEvent}
+            >
+              <Collapsible.Trigger className="flex flex-row justify-center items-center gap-x-2 border border-[#D4D4D4] rounded-md bg-white">
+              Event Type
+                {openEvent ? <Cross2Icon /> : <CaretDownIcon />}
+              </Collapsible.Trigger>
+                
+                  
+              <Collapsible.Content className="flex flex-col flex-start border border-[#D4D4D4] rounded-md gap-y-1 text-left p-4 mt-3 ">
+                {eventTypes.map((eventID) => (
+                  <div className="flex flex-row items-center gap-x-2">
+                    <input
+                      type="checkbox"
+                      value={eventID}
+                      onChange={(e) => filterUpdate(e, eventFilter, setEventFilter)}
+                      id={eventID}
+                    />
+                    <p>{eventID}</p>
+                  </div>
+                ))}
+              </Collapsible.Content>
+            </Collapsible.Root>   
             </div>
-            <div className="flex flex-row items-center gap-x-2">
-              <input
-                type="checkbox"
-                value="neutral"
-                onChange={filterChange}
-                id="neutral"
-              />
-              <p>Neutral</p>
-            </div>
-            <div className="flex flex-row items-center gap-x-2">
-              <input
-                type="checkbox"
-                value="negative"
-                onChange={filterChange}
-                id="negative"
-              />
-              <p>Negative</p>
-            </div>
-          
-          </div>
+
           
               
               <div className="flex flex-col w-full md:mt-15">
