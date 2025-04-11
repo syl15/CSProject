62b421168c103169d0f6d19933721f064836c7e8
Sneha Bista <snehabista727@gmail.com>
2025-04-11 12:40:44 -0500

    updated spacing for total tweets

--- frontend/src/components/TotalTweets.jsx
+++ frontend/src/components/TotalTweets.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function TotalTweets({total}) {
   console.log
   return (
-    <div className="flex flex-col flex-start border border-[#D4D4D4] rounded-md mt-15 text-left p-4">
+    <div className="flex flex-col flex-start border border-[#D4D4D4] rounded-md mt-5 md:mt-15 text-left p-4">
         <h3>Total Posts</h3>
         <h1 className="text-3xl font-bold mt-2">
           {total ? (
