1277105bfcd0d8e5b1e6739a4a8dc2b211c3865f
Sneha Bista <snehabista727@gmail.com>
2025-04-14 15:07:05 -0500

    fixed search bar formatting

--- frontend/src/components/SearchResultsList.jsx
+++ frontend/src/components/SearchResultsList.jsx
@@ -3,7 +3,7 @@ import SearchResult from './SearchResult';
 
 export default function SearchResultsList( {results}) {
     return (
-    <div className = "results-list">
+    <div className = "results-list fixed -mt-[0.5]">
         { results.map((result, id) => {
             return <SearchResult result = {result} key = {id}/>; 
         })}
