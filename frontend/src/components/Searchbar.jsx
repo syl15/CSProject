3978eeebea0df6df6002018b6e14883b86306c1e
laurennicolas <71050445+laurennicolas@users.noreply.github.com>
2025-04-15 15:44:08 -0500

    added functional searchbar

--- frontend/src/components/Searchbar.jsx
+++ frontend/src/components/Searchbar.jsx
@@ -1,33 +1,22 @@
 import React, {useState} from 'react';
 import { MagnifyingGlassIcon } from '@radix-ui/react-icons';
-//const BASE_URL = "http://127.0.0.1:5001"; // Flask API URL 
+
+import { BASE_URL } from "../config.js";
 
 export default function Searchbar({ setResults }) {
     const [input, setInput] = useState("");
-    //const [disasterInfo, setDisasterInfo] = useState("");
-
     
     const fetchData = (value) => {
-        fetch("https://jsonplaceholder.typicode.com/users") // http://127.0.0.1:5001
+        fetch(`${BASE_URL}/disasters`) 
             .then((response) => response.json())
             .then((json) => {
-                const results = json.filter((user) => (
-                    value && user && user.name && 
-                    user.name.toLowerCase().includes(value.toLowerCase())
+                const results = json.filter((disaster) => (
+                    value && disaster.name && 
+                    disaster.name.toLowerCase().includes(value.toLowerCase())
                 ));
                 setResults(results);
             });
-/*
-            const fetchData = (value) => {
-              fetch("BASE_URL/disasters") // Flask API URL
-                  .then((response) => response.json())
-                  .then((json) => {
-                      const disasterList = json.filter((disaster) => (
-                          value && disaster && disaster.name && 
-                          disaster.name.toLowerCase().includes(value.toLowerCase())
-                      ));
-                      setResults(disasterList);
-                  });*/
+        
     };
 
     const handleChange = (value) => {
@@ -39,7 +28,7 @@ export default function Searchbar({ setResults }) {
         <div className="relative">
             <MagnifyingGlassIcon className="absolute left-3 top-[11px]"/>
             <input
-                className="h-[38px] w-full pl-8 pr-2 border border-[#D4D4D4] rounded-sm text-sm focus:outline-0"
+                className="h-[38px] w-full pl-8 pr-2 border border-[#D4D4D4] text-sm"
                 type="text"
                 placeholder="Search"
                 value={input}
