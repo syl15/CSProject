d44dddfc32a022de1a25a9f2c1785c6cd27a401a
Sneha Bista <snehabista727@gmail.com>
2025-04-15 12:18:30 -0500

    updated tweets to post and added loading screen for all disasters page

--- frontend/src/components/TopTweets.jsx
+++ frontend/src/components/TopTweets.jsx
@@ -6,7 +6,7 @@ export default function TopTweets({tweetsList}) {
   return (
     <div className="flex flex-col border border-[#D4D4D4] rounded-md mt-5 md:mt-15 text-left p-4 gap-y-2 h-[25rem] w-full">
         <div className="border-b-1 border-[#D4D4D4]">
-            <h3 className="pb-2">Tweets</h3>
+            <h3 className="pb-2">Posts</h3>
         </div>
         
         
