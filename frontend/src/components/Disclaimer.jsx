f697192068dcd3d7eb0cac24759c8273c35bc952
Sneha Bista <snehabista727@gmail.com>
2025-04-17 14:39:25 -0500

    fixed border

--- frontend/src/components/Disclaimer.jsx
+++ frontend/src/components/Disclaimer.jsx
@@ -7,7 +7,7 @@ export default function Disclaimer() {
     //edf5ff
     
   return (
-    <div className="flex flex-col flex-start justify-center bg-[#edf5ff] border border-1 border-[#0644cf] rounded-md text-left p-6 gap-y-5 min-w-screen absolute left-0 right-0 max-h-10 mb-10">
+    <div className="flex flex-col flex-start justify-center bg-[#edf5ff] border border-1 border-b-[#0644cf] border-t-[#0644cf] text-left p-6 gap-y-5 min-w-screen absolute left-0 right-0 max-h-10 mb-10">
         <div className="flex flex-row items-center gap-x-2 pl-3 md:pl-10">
             <InfoCircledIcon className="fill-[#0644cf]"/>
             This is the most recent disaster.
