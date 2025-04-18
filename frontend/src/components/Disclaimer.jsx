6e80456cd5a4109e220ed8a2b5262a2be7cf27c8
Sneha Bista <snehabista727@gmail.com>
2025-04-17 20:30:03 -0500

    updated disclaimer

--- frontend/src/components/Disclaimer.jsx
+++ frontend/src/components/Disclaimer.jsx
@@ -10,7 +10,7 @@ export default function Disclaimer() {
     <div className="flex flex-col flex-start justify-center bg-[#edf5ff] border border-1 border-b-[#0644cf] border-t-[#0644cf] text-left p-6 gap-y-5 min-w-screen absolute left-0 right-0 max-h-10 mb-10">
         <div className="flex flex-row items-center gap-x-2 pl-3 md:pl-10">
             <InfoCircledIcon className="fill-[#0644cf]"/>
-            This is the most recent disaster.
+            This is the most recent disaster that we have data on.
         </div>
     </div>
   );
