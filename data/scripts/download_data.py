59de04af80ba7508312ba813634e188e13691a03
kastatine <47929681+kastatine@users.noreply.github.com>
2025-04-20 16:40:02 -0500

    updated data/scripts to match report branch

--- data/scripts/download_data.py
+++ data/scripts/download_data.py
@@ -1,19 +1,19 @@
 import os 
 import gdown 
 
-# Downloads train, test, dev sets from local ../datasets directory (https://drive.google.com/drive/u/5/folders/1_RTvKXflGBOdWWaocSOax5JxPzBIz8n1)
-train_id = "1ABaDbbrpJvLR-mVX5KW9vXnRuQNufZuQ"
-test_id = "1D0H5ASAat1sDzTuqB9VEJozvwrCKQ04w"
-dev_id = "1MEx0KFVDUHaWWfWK4XGc_Ch4zaTmcqsh"
+# Downloads crisisnlp train, test, dev sets from google drive (https://drive.google.com/drive/folders/1Gmm5frRwloIV6Ur5yoCPjKRJ31UnQ1RY) to local datasets/combined_datasets directory
+train_id = "1051g1Y5xUc716CR5bqeaVYH0D-cFnm5I"
+test_id = "100ck2eOW37Sg7o6SFu_rqwM6XnQnYosA"
+dev_id = "1-sXOLEh10vnIa_FgVWOCOqBmC37acpMu"
 
 files_info = [
     {"url": f"https://drive.google.com/uc?id={train_id}", "filename": "train.tsv"}, 
     {"url": f"https://drive.google.com/uc?id={test_id}", "filename": "test.tsv"},
-    # {"url": f"https://drive.google.com/uc?id={dev_id}", "filename": "dev.tsv"}
+    {"url": f"https://drive.google.com/uc?id={dev_id}", "filename": "dev.tsv"}
 ]
 
 # Relative path to data directory 
-data_directory = "datasets" 
+data_directory = "../datasets/combined_datasets" 
 
 # If data folder does not exist, create it
 if not os.path.exists(data_directory):
@@ -24,6 +24,6 @@ for file_info in files_info:
     url = file_info["url"]
     filename = file_info["filename"]
     print(f"Downloading {url} as {filename}...")
-    gdown.download(url, os.path.join(data_directory, filename), quiet=False)
+    gdown.download(url, os.path.join(data_directory, f"combined_{filename}"), quiet=False)
 
 print("Download complete")
